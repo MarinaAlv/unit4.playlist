5697391de6774d1bc00e1b5f3ac756a58b415105
Marina <marvarado19@gmail.com>
2024-10-07 10:21:12 -0700

    doneclassexample

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
-
 const PORT = 3000;
+const playlist = require('./playlist');
 
 app.get('/', (req, res) => {
   res.send("You've reached the Playlist API!");
+});
+
+app.get('/playlist', (req, res) => {
+  res.json(playlist);
+});
+
+app.get('/playlist/:index', (req, res) => {
+  const {index} = req.params;
+  if (index < 0 || index >= playlist.length) {
+    res.status(404).send('That song does not exist in the playlist.');
+  } else {
+    res.json(playlist[index]);
+  }
 });
 
 app.listen(PORT, () => {
